db6d4a646e8a9806ec05f75128ca112cfed4b0ff
Ebensantosa <ebensantosa9@gmail.com>
2020-09-30 22:43:19 +0700

    - Simple Change

--- app/src/main/java/com/hallo/helloworld/MainActivity.java
+++ app/src/main/java/com/hallo/helloworld/MainActivity.java
@@ -3,12 +3,15 @@ package com.hallo.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate: dipanggil");
         setContentView(R.layout.activity_main);
     }
 }

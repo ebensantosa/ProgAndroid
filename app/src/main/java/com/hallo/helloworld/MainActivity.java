cd9ae60f9d4547bfb446b9cf4fb09fa3472bcb11
Ebensantosa <ebensantosa9@gmail.com>
2020-09-30 23:21:50 +0700

    - Simple Change

--- app/src/main/java/com/hallo/helloworld/MainActivity.java
+++ app/src/main/java/com/hallo/helloworld/MainActivity.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
     private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "onCreate: dipanggil");
+        Log.i(TAG, "onCreate: perubahan");
         setContentView(R.layout.activity_main);
     }
 }
